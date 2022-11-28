4b29d1182f39efa85efa0c9ca8da4515f733df30
windmemory <wind.memory.cn@gmail.com>
2022-11-29 03:42:09 +0800

    fix build

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -3,12 +3,7 @@ set -e
 
 imageName='juzi-marketing-bot'
 ECR_URL=789252305933.dkr.ecr.cn-northwest-1.amazonaws.com.cn
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
+PACKAGE_VERSION=$IMAGE_TAG
 
 echo current package version: "$PACKAGE_VERSION"
 
