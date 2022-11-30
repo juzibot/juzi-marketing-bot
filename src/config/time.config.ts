aa564c4296d1ecf4eec1aeac106c328c090ad6f9
comeonzhj <comeonzhj@163.com>
2022-11-30 21:31:06 +0800

    Update time.config.ts
    更新成正式推送的时间

--- src/config/time.config.ts
+++ src/config/time.config.ts
@@ -10,15 +10,15 @@ export interface TimeConfig {
 
 export const timeConfig: TimeConfig = {
   // 早报定时发送时间
-  morningReport: '20:55',
+  morningReport: '07:30',
   // 早上油价定时发送时间
-  morningGasMessage: '20:57',
+  morningGasMessage: '08:00',
   // 新闻定时发送时间
-  newsMessage: '21:00',
+  newsMessage: '09:00',
   // 开心一刻发送时间
-  jokeMessage: '21:02',
+  jokeMessage: '16:00',
   // 下午油价定时发送时间
-  afternoonGasMessage: '21:05',
+  afternoonGasMessage: '20:00',
   // 发消息的消息之间的间隔，单位秒
   messageInterval: 0.5,
   // 发消息群之间的间隔，单位秒
