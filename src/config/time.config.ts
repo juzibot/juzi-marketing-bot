2cef69e7a292aceae84beb4effc521f1a3bb0048
comeonzhj <comeonzhj@163.com>
2022-12-01 16:34:56 +0800

    Update time.config.ts
     更新下午油价发送时间

--- src/config/time.config.ts
+++ src/config/time.config.ts
@@ -18,7 +18,7 @@ export const timeConfig: TimeConfig = {
   // 开心一刻发送时间
   jokeMessage: '16:00',
   // 下午油价定时发送时间
-  afternoonGasMessage: '20:00',
+  afternoonGasMessage: '17:00',
   // 发消息的消息之间的间隔，单位秒
   messageInterval: 0.5,
   // 发消息群之间的间隔，单位秒
